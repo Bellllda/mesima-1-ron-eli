b84e14ac29a1ec51878c068e6e6c376a218464f4
Bellllda <ronelironeliron@gmail.com>
2020-06-10 11:16:51 +0300

    almost end

---
+++ radius.js
@@ -0,0 +1,35 @@
+
+var canvas = document.getElementById("canvas");
+canvas.width = 500;
+canvas.height = 400;
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
+var ctx = canvas.getContext("2d");
+
+var calculate = document.getElementById("calc");
+
+//calculating the vol onclick
+calculate.onclick = function (e) {
+    var rad = document.getElementById("radius").value;
+    var radius = parseInt(rad)
+    if (radius > 200) {
+        alert("The radius is bigger then the canvas! please rewrite the radius with a smaller radius");
+
+    }
+    else {
+        //Volume Calc
+        var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+            document.getElementById("vol").innerHTML = volume
+
+        ctx.beginPath();
+        ctx.arc(centerX,centerY, radius, 0, 2 * Math.PI)    
+        ctx.stroke();
+
+
+    }
+
+}
+var clear = document.getElementById("clear");
+clear.onclick = function (cl) {
+    ctx.clearRect(0 , 0 , 500 , 400)
+}
